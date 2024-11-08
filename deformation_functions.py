3fbb4a90f1534b23d412c7e94f71be6d16a6eef9
Seb (MBP) <seb@eskd.net>
2024-11-08 14:48:05 +0100

    Initial commit

---
+++ deformation_functions.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+# Creating a dataframe - don't worry too much about this for now
+df = pd.DataFrame({'r': [0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 10, 15, 20, 25, 30], 'ur_m': [0]*16, 'uz_m': [0]*16, 'ur_cm': [0]*16, 'uz_cm': [0]*16})
+# Displaying the dataframe
